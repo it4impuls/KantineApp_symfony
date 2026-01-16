7718da9d4cf6989059551fa16f0191319fc63a48
Marilynias <marilynia57@gmail.com>
2026-01-16 09:03:16 +0100

    rename admin group dont deploy sql files

---
+++ InsertData.sql
@@ -0,0 +1,29 @@
+--
+-- Dumping data for table `Costumer`
+--
+INSERT INTO
+  `Costumer` (
+    `firstname`,
+    `lastname`,
+    `id`,
+    `active`,
+    `enddate`
+  )
+VALUES
+  ---- DATA ----
+;
+
+--
+-- Dumping data for table `order`
+--
+INSERT INTO
+  `order` (
+    `id`,
+    `order_dateTime`,
+    `ordered_item`,
+    `tax`,
+    `Costumer_id`
+  )
+VALUES
+  ---- DATA ----
+;
