256ef4b24a0991d63305ab2fc0a3f8fea1e4f8ed
Marilynias <marilynia57@gmail.com>
2026-04-13 14:31:15 +0200

    add admin view tests

--- apps/zeiterfassung/tests/TimeEntryTest.php
+++ apps/zeiterfassung/tests/TimeEntryTest.php
@@ -27,6 +27,26 @@ class TimeEntryTest extends WebTestCase
         return new static::$class($env, $debug, 'zeiterfassung');
     }
 
+    /** I have no idea why $this->client->loginUser($adminUser) is not enough, but we have to manually submit the form from /login */
+    private function authenticate(): void
+    {
+        $userRepository = $this->getContainer()->get(SonataUserUserRepository::class);
+        $adminUser = $userRepository->findOneByUsername('admin');
+        $this->client->loginUser($adminUser);
+        // 
+
+        // $this->loginUser()
+        $this->client->request('GET', '/admin/logout', );
+        $this->client->request('GET', '/admin/login', );
+        var_dump($this->client->getResponse()->getContent());
+        $this->assertResponseIsSuccessful('Could not get login page');
+        $this->client->submitForm('submit', [
+            "_username"=>	"admin",
+            "_password"=>	"admin"
+        ]);
+        var_dump($this->client->getResponse());
+    }
+
     public function setUp(): void
     {
         $this->client = static::createClient();
@@ -129,7 +149,41 @@ class TimeEntryTest extends WebTestCase
         $this->assertTrue($time_to_post === $res["time"], "posted time ".$time_to_post." is not the same as saved time ".$res["time"] ?? $this->client->getResponse()->getContent());
     }
 
+    public function testTestAdminGet(): void
+    {
+        $this->authenticate();
 
+        $this->client->request('GET', '/admin/dashboard', );
+        $this->assertResponseIsSuccessful('Could not get admin main page: '.$this->client->getResponse()->getStatusCode());
+
+        foreach (["shared/sonatauseruser", "shared/costumer", "attendance", "zeiterfassung/fauser"] as $key) {
+            $this->client->request('GET', '/admin/'.$key.'/list', );
+            $this->assertResponseIsSuccessful('Could not get '.$key.' page: '.$this->client->getResponse()->getStatusCode());
+            $crawler = $this->client->getCrawler();
+            
+            // view
+            $entries = $crawler->filter('.sonata-link-identifier');
+            if(count($entries) > 0){
+                $this->client->click($entries->first()->link());
+                $this->assertResponseIsSuccessful('Could get entity '.$key.' page: '.$this->client->getResponse()->getStatusCode());
+                var_dump($this->client->getResponse()->getContent());
+            }
+
+            // edit
+            $entries = $crawler->filter('.edit_link');
+            if(count($entries) > 0){
+                $this->client->click($entries->first()->link());
+                $this->assertResponseIsSuccessful('Could get entity '.$key.' page: '.$this->client->getResponse()->getStatusCode());
+            }
+
+            // create
+            $entries = $crawler->filter('.sonata-action-element');
+            if(count($entries) > 0){
+                $this->client->click($entries->first()->link());
+                $this->assertResponseIsSuccessful('Could get entity '.$key.' page: '.$this->client->getResponse()->getStatusCode());
+            }
+        }
+    }
 }
 
 
